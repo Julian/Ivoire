8e86ecb528e3fe21aee5ec167d8ced866e995efd
Julian Berman <Julian+git@GrayVines.com>
2012-09-20 15:11:29 -0400

    Port transform specs.

---
+++ ivoire/spec/unit/transform_spec.py
@@ -0,0 +1,87 @@
+from __future__ import print_function
+from textwrap import dedent
+from unittest import TestCase
+import ast
+import copy
+
+from ivoire import describe, transform
+from ivoire.spec.util import mock, patch, patchObject
+
+
+def dump(node):  # pragma: no cover
+    return(dedent("""
+    --- Dumping Node ---
+
+    {}
+
+    --- End ---
+    """.format(ast.dump(node))))
+
+
+with describe(transform.ExampleTransformer) as it:
+    @it.before
+    def before(test):
+        test.transformer = transform.ExampleTransformer()
+
+    def execute(test, source):
+        test.globals, test.locals = {}, {}
+        test.node = ast.parse(dedent(source))
+        test.transformed = test.transformer.transform(test.node)
+        compiled = compile(test.transformed, "<testing transform>", "exec")
+
+        try:
+            exec(compiled, test.globals, test.locals)
+        except Exception:  # pragma: no cover
+            print(dump(test.transformed))
+            raise
+
+    def assertNotTransformed(test, source):
+        node = ast.parse(dedent(source))
+        transformed = test.transformer.transform(copy.deepcopy(node))
+        # TODO: Ugly! Fix me please. See http://bugs.python.org/issue15987
+        test.assertEqual(ast.dump(node), ast.dump(transformed))
+
+    with it("fixes missing line numbers") as test:
+        fix = patchObject(test, transform.ast, "fix_missing_locations")
+        node = ast.Pass()
+        test.transformer.transform(node)
+        fix.assert_called_once_with(node)
+
+    with it("transforms ivoire imports to unittest imports") as test:
+        execute(test, "from ivoire import describe")
+        test.assertEqual(test.locals, {"TestCase" : TestCase})
+
+    with it("leaves other imports alone") as test:
+        assertNotTransformed(test, "from textwrap import dedent")
+
+    with it("transforms uses of describe to TestCases") as test:
+        test.skip_if(
+            not transform.transform_possible,
+            "Transform not available on this version."
+        )
+
+        execute(test, """
+            from ivoire import describe
+            with describe(next) as it:
+                with it("returns the next element") as test:
+                    test.i = [1, 2, 3]
+                    test.assertEqual(next(test.i), 1)
+        """)
+
+        TestNext = test.locals["TestNext"]
+        test = TestNext("test_it_returns_the_next_element")
+        test.run()
+        test.assertEqual(test.i, [1, 2, 3])
+
+    with it("leaves other context managers alone") as test:
+        test.skip_if(
+            not transform.transform_possible,
+            "Transform not available on this version."
+        )
+
+        assertNotTransformed(test, """
+            from warnings import catchwarnings
+            with catchwarnings() as thing:
+                with catchwarnings():
+                    pass
+        """)
--- ivoire/tests/test_transform.py
+++ ivoire/tests/test_transform.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 from textwrap import dedent
-from unittest import TestCase, TestLoader, skipIf
+from unittest import TestCase, skipIf
 import ast
 
 from ivoire import transform
@@ -65,76 +65,3 @@ class TestExampleLoader(TestCase, PatchMixin):
             trans.return_value, path, "exec", dont_inherit=True
         )
         trans.assert_called_once_with(parse.return_value)
-
-
-class TestExampleTransformer(TestCase, PatchMixin):
-    def setUp(self):
-        self.transformer = transform.ExampleTransformer()
-
-    def assertNotTransformed(self, source):
-        node = ast.parse(dedent(source))
-        transformed = self.transformer.transform(node)
-        self.assertIs(node, transformed, "unexpected transformation!")
-
-    def dump(self, node):  # pragma: no cover
-        print(
-            "\n--- Dumping Node ---\n",
-            ast.dump(node),
-            "\n--- End ---",
-            sep="\n"
-        )
-
-    def execute(self, source):
-        self.globals, self.locals = {}, {}
-        self.node = ast.parse(dedent(source))
-        self.transformed = self.transformer.transform(self.node)
-        compiled = compile(self.transformed, "<testing transform>", "exec")
-
-        try:
-            exec(compiled, self.globals, self.locals)
-        except Exception:  # pragma: no cover
-            self.dump(self.transformed)
-            raise
-
-    def test_it_fixes_missing_linenos(self):
-        fix = self.patchObject(transform.ast, "fix_missing_locations")
-        node = ast.Pass()
-        self.transformer.transform(node)
-        fix.assert_called_once_with(node)
-
-    def test_it_transforms_ivoire_imports_to_unittest(self):
-        self.execute("from ivoire import describe")
-        self.assertEqual(self.locals, {"TestCase" : TestCase})
-
-    def test_it_does_not_transform_other_imports_to_unittest(self):
-        self.assertNotTransformed("from textwrap import dedent")
-
-    @skipIf(
-        not transform.transform_possible,
-        "Transformation isn't supported yet on this version.",
-    )
-    def test_it_transforms_uses_of_describe_to_test_cases(self):
-        self.execute("""
-            from ivoire import describe
-            with describe(next) as it:
-                with it("returns the next element") as test:
-                    test.i = [1, 2, 3]
-                    test.assertEqual(next(test.i), 1)
-        """)
-
-        TestNext = self.locals["TestNext"]
-        test = TestNext("test_it_returns_the_next_element")
-        test.run()
-        self.assertEqual(test.i, [1, 2, 3])
-
-    @skipIf(
-        not transform.transform_possible,
-        "Transformation isn't supported yet on this version.",
-    )
-    def test_it_does_not_transform_other_context_managers(self):
-        self.assertNotTransformed("""
-            from warnings import catchwarnings
-            with catchwarnings() as thing:
-                with catchwarnings():
-                    pass
-        """)
